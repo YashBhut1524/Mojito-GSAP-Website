4ab5ed125b655843dfc481d7d7b9729610398053
YaSh1524 <yashxerox15@gmail.com>
2025-07-30 22:22:26 +0530

    add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div>
-      Hellow
-    </div>
+    <main>
+      <Navbar />
+      {/* <Hero /> */}
+    </main>
   )
 }
 
